09570b5ad7f9f90b52a5e0cf460fda1d36646bbb
Messier1071 <ian.marmendes@gmail.com>
2026-08-15 20:45:03 -0300

    commit

---
+++ penny/main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from fastapi import Response
+
+app = FastAPI()
+
+
+@app.get("/status/")
+async def root():
+    res = Response(
+        content='{"message": "Hey cass"}',
+        status_code=418,
+        headers=None,
+        media_type="application/json",
+        background=None,
+    )
+    return res
+
+@app.get("/status/webapp")
+async def root():
+    res = Response(
+        content='{"message": "Hey cass"}',
+        status_code=418,
+        headers=None,
+        media_type="application/json",
+        background=None,
+    )
+    return res
+
+@app.get("/status/database")
+async def root():
+    res = Response(
+        content='{"message": "Hey cass"}',
+        status_code=418,
+        headers=None,
+        media_type="application/json",
+        background=None,
+    )
+    return res
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
